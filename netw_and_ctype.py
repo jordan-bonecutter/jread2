ceb0c317980c574f321633a6afc6d309a69daf62
Jordan Bonecutter <jordanbonecutter@gmail.com>
2019-10-12 16:46:30 -0700

    added some comments to netw_and_ctype

--- netw_and_ctype.py
+++ netw_and_ctype.py
@@ -6,16 +6,19 @@
 from nodetype import nodetype
 
 def get_netw_time(crawl) -> dict:
+  # return dictionary
   ret = {}
-  t, f = 0, 0
+
+  # loop thru the whole crawl
   for site, data in crawl.items():
+    # foreach site, record total time and time for ads
     ret[site] = {'total_time': 0, 'ad_time': 0, 'ad_plus_inherited': 0}
     for snap in data['snapshots']:
       for layer in snap['tree_full']:
         for url, info in layer.items():
-          t += 1
+          # if the current node has no timing info (< 2%)
+          # then skip it
           if 'timing' not in info:
-            f += 1
             continue
           ret[site]['total_time'] += info['timing'] / len(data['snapshots'])
           if info['ad'] != 'no':
@@ -23,6 +26,7 @@ def get_netw_time(crawl) -> dict:
             if info['ad'] == 'yes':
               ret[site]['ad_time'] += info['timing'] / len(data['snapshots'])
 
+  # calculate the ratios
   for site in ret.keys():
     try:
       ret[site]['ad_ratio'] = ret[site]['ad_time']/ret[site]['total_time']
@@ -33,16 +37,21 @@ def get_netw_time(crawl) -> dict:
     except ZeroDivisionError:
       ret[site]['ad_plus_inherited_ratio'] = 0.
 
-  return ret, f/t
+  return ret
 
 def get_num_ctypes(crawl) -> dict:
+  # return dictionary
   ret = {}
+
+  # loop thru the crawls
   for site, data in crawl.items():
     for snap in data['snapshots']:
       for layer in snap['tree_full']:
         for url, info in layer.items():
+          # get the content type
           ctype = nodetype(info)
           if ctype not in ret:
+            # for each content type, gather total# and #related to ads
             ret[ctype] = {'total': 0., 'ad': 0., 'ad_plus_inherited': 0.}
           ret[ctype]['total'] += 1./len(data['snapshots'])
           if info['ad'] != 'no':
