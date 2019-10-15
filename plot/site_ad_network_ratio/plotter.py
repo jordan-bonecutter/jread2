a1456219dc37c4e90fd0d72a97938a0df3d2249a
Jordan Bonecutter <jordanbonecutter@gmail.com>
2019-10-14 19:14:27 -0700

    Updated plotter for site network ratio

--- plot/site_ad_network_ratio/plotter.py
+++ plot/site_ad_network_ratio/plotter.py
@@ -1,6 +1,5 @@
 import json
-import matplotlib
-#matplotlib.use('agg')
+import matplotlib as mpl
 import matplotlib.pyplot as plt
 
 import numpy as np
@@ -33,27 +32,32 @@ for site, time in stat.items():
   ratio_ustop200_1level.append(time['ad_plus_inherited_ratio'])
 
 n_bins = 10000  
+fig, ax = plt.subplots()
+plt.xticks(np.arange(0, 1.1, step=0.1))
+plt.yticks(np.arange(0, 1.1, step=0.1))
+plt.grid(b=True, which='major', color='gray', linestyle='-', alpha=0.2)
+plt.grid(b=True, which='minor', color='gray', linestyle='-', alpha=0.2)
+
 n, bins, patches = plt.hist(ratio_news, n_bins, density=True, histtype='step',
-                           cumulative=True, label='News')
+                           cumulative=True, label='News (landing)', range=(0, 1))
 
 n, bins, patches = plt.hist(ratio_news_1level, n_bins, density=True, histtype='step',
-                           cumulative=True, label='News_1L')
+                           cumulative=True, label='News (post-click)', range=(0, 1))
 
 n, bins, patches = plt.hist(ratio_ustop200, n_bins, density=True, histtype='step',
-                           cumulative=True, label='top200')
+                           cumulative=True, label='General (landing)', range=(0, 1))
 
 n, bins, patches = plt.hist(ratio_ustop200_1level, n_bins, density=True, histtype='step',
-                           cumulative=True, label='top200_1L')
+                           cumulative=True, label='General (post-click)', range=(0, 1))
 
-plt.legend()
+plt.xlabel('Network Cost Ratio of Ads')
+plt.ylabel('Fraction of Websites')
+plt.title('Ads Network Cost (CDF)') 
+plt.legend(loc='lower right')
 
-plt.xlabel('network cost ratio of ads')
-plt.ylabel('Fraction of websites')
-plt.title('ads network cost (CDF)') 
-
+axpolygons = [poly for poly in ax.get_children() if isinstance(poly, mpl.patches.Polygon)]
+for poly in axpolygons:
+  poly.set_xy(poly.get_xy()[:-1])
 
 plt.savefig('network_cost_cdf', format='pdf')
 plt.show()
-
-
-
