05d0c2ccfd405b3a70e19d4a5b8a5e758aa2a05d
Jordan Bonecutter <jordanbonecutter@gmail.com>
2019-10-14 17:30:21 -0700

    Updated plotter for adblocker overhead

--- plot/adblocker_overhead/plotter.py
+++ plot/adblocker_overhead/plotter.py
@@ -1,62 +1,74 @@
 import json
-import matplotlib
-#matplotlib.use('agg')
+import matplotlib as mpl
 import matplotlib.pyplot as plt
-
 import numpy as np
 
-def get_time(word):
-  return float( word.split()[1].split("m")[0])
-def get_request(word):
-  return int(word.split(":")[1])
+with open('ext_info8.json', 'r') as fi:
+  ext_info = json.load(fi)
+with open('nxt_info8.json', 'r') as fi:
+  nxt_info = json.load(fi)
 
-with open('nxt_info8.json','r') as f:
-  vanilla = json.load(f)
-with open('ext_info8.json','r') as f:
-  blocking = json.load(f)
+ext_times = {}
+ext_avg = 0.
+for key, items in ext_info.items():
+  val = 0.
+  n   = 0
+  for item in items:
+    if 'load' in item:
+      val += float(item[6:-2]) 
+      n   += 1
+  try:
+    ext_times[key] = val / n
+    ext_avg += val / n
+  except ZeroDivisionError:
+    pass
+ext_avg /= len(ext_times)
 
-x = []
-y = []
+nxt_times = {}
+nxt_avg = 0.
+for key, items in nxt_info.items():
+  val = 0.
+  n   = 0
+  for item in items:
+    if 'load' in item:
+      val += float(item[6:-2])     
+      n   += 1 
+  try:
+    nxt_times[key] = val / n
+    nxt_avg += val / n
+  except ZeroDivisionError:
+    pass
+nxt_avg /= len(nxt_times)
 
-vanillaStat = {}
-for site in vanilla.keys():
-  time = 0.0
-  requests = 0
-  n = 0
-  for item in vanilla[site]:
-    if item.find("load") >= 0:
-      time += get_time(item)
-      n += 1  
-    elif item.find("request") >= 0:
-      requests += get_request(item)
-  if n :
-    #print (time/n, float(requests)/n)
-    vanillaStat[site] = {'time':time/n, 'requests':float(requests)/n}
-blockingStat = {}
-for site in blocking.keys():
-  time = 0.0
-  requests = 0
-  n = 0
-  for item in blocking[site] :
-    if item.find("load") >= 0:
-      time += get_time(item)
-      n += 1  
-    elif item.find("request") >= 0:
-      requests += get_request(item)
-  if n :
-    #print (time/n, float(requests)/n)
-    blockingStat[site] = {'time':time/n, 'requests':float(requests)/n}
+tohist = []
+for key, val in ext_times.items():
+  try:
+    tohist.append(100*(val/nxt_times[key] - 1.))
+  except KeyError:
+    pass
+tohist = sorted(tohist)
 
-for site in vanillaStat.keys():
-  if site in blockingStat.keys():
-    y.append((blockingStat[site]['time'] - vanillaStat[site]['time'])/blockingStat[site]['time'])
-    x.append(vanillaStat[site]['requests'])
+fig, ax = plt.subplots()
+plt.xticks(np.arange(0, 240, step=20))
+plt.yticks(np.arange(0, 1.1, step=0.1))
+plt.grid(b=True, which='major', color='gray', linestyle='-', alpha=0.2)
+plt.grid(b=True, which='minor', color='gray', linestyle='-', alpha=0.2)
 
-#x = np.linspace(0, 10, 30)
-#y = np.sin(x)
+n_bins = len(tohist)*3
 
-plt.plot(x, y, 'o', color='black')
+n, bins, patches = plt.hist(tohist, n_bins, density=True, histtype='step',
+                           cumulative=True, label='With Modified Adblocker')
+
+plt.legend(loc='lower right')
+
+plt.xlabel('% Overhead for Page Load Time')
+plt.ylabel('Fraction of Websites')
+plt.title('CDF of % Overhead for AdBlockers') 
+
+
+axpolygons = [poly for poly in ax.get_children() if isinstance(poly, mpl.patches.Polygon)]
+for poly in axpolygons:
+  poly.set_xy(poly.get_xy()[:-1])
+
+plt.savefig('adblocker_overhead', format='pdf')
 plt.show()
-
-
-
