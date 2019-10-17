eb80cb805e2a6da0ec7260c8ee2fc2f825525f09
Octavi Obiols <octaviobiols@dhcp-v033-045.mobile.uci.edu>
2019-10-17 12:44:16 -0700

    update plots

--- plot/domain_ad_performance_ratio/plotter.py
+++ plot/domain_ad_performance_ratio/plotter.py
@@ -13,23 +13,31 @@ with open('domain_stat_news_t.json','r') as f:
 
 sortedDomain = sorted(stat.items(), key=lambda x: x[1]['ratio_to_ads'], reverse=True)
 
+
+
 topDomains = sortedDomain[0:10]
 
 label = []
 val = []
-
+pop = []
 for domain, ratio in topDomains:
   label.append(domain)
   val.append(ratio['ratio_to_ads'])
+  pop.append(stat[domain]['number_of_websites'])
 
 #print (sortedDomain)
 
 l = np.arange(len(label))
-plt.bar(l, val, color='darkgrey')
+bar = plt.bar(l, val, color='darkgrey')
+for i, rect in enumerate(bar):
+    height = rect.get_height()
+    plt.text(rect.get_x() + rect.get_width()/2.0, height, '%d' % pop[i], ha='center', va='bottom')
+
 y_pos = range(len(label))
 plt.xticks(y_pos, label, fontsize=8, rotation=50, ha='right')
-plt.ylabel('Fraction')
-plt.title('Contribution of ad domains in performance cost')
+plt.ylabel('Ratio')
+plt.ylim([0,0.25])
+plt.title('Contribution of ad domains to performance cost')
 plt.tight_layout()
 plt.savefig('domain_contribution', format='pdf')
 plt.show()
